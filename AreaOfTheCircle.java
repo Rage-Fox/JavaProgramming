472e8e5e5d3106e80b75cb889ac6f89e2bf1c8dc
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2023-02-07 12:01:54 +0530

    Add files via upload

---
+++ AreaOfTheCircle.java
@@ -0,0 +1,15 @@
+public class AreaOfTheCircle{
+    public static void main(String args[])
+    {
+        float pi=3.14f,area;//3.14 means double and 3.14f means float
+        int r=8;
+        area=pi*r*r;
+        System.out.println(area);
+    }
+}
+/*
+Compilation and Execution through command prompt:
+Compilation: javac Filename.java
+Execution: java Filename
+**Note: Always first compile the code before executing**
+*/
