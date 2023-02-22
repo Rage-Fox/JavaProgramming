b884a411d618ee07fb3b02056036b3f4656edf9b
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2023-02-22 11:17:17 +0530

    Add files via upload

---
+++ BookInformationUsingClasses.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class BookInformationUsingClasses {
+    //default access specifier variables
+    String bid,bname,author;
+    int no_of_pages;
+    double cost;
+    Scanner sc=new Scanner(System.in);
+    public void get_book_info()
+    {
+        bid=sc.nextLine();
+        bname=sc.nextLine();
+        author=sc.nextLine();
+        no_of_pages=sc.nextInt();
+        cost=sc.nextDouble();
+    }
+    public void show_book_info()
+    {
+        System.out.println(bid+" "+bname+" "+author+" "+no_of_pages+" "+cost);
+    }
+    public static void main(String args[])
+    {
+        BookInformationUsingClasses b1=new BookInformationUsingClasses();
+        b1.get_book_info();
+        b1.show_book_info();
+    }
+}
