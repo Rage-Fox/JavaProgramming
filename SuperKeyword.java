677e874e12b353ae523a1e1314f1d2550698463c
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2023-03-07 10:40:29 +0530

    Add files via upload

---
+++ SuperKeyword.java
@@ -0,0 +1,32 @@
+class SA
+{
+    int x=10;
+    public void display()
+    {
+        System.out.println("Parent A: "+x);
+    }
+}
+class SB extends SA
+{
+    int x=20;
+    public void display()
+    {
+        super.display();
+        System.out.println("Child B: "+x);//refers x value from same class.
+        System.out.println("Child A: "+super.x);//refers x value from the parent SA class.
+    }
+}
+public class SuperKeyword {
+    /*
+    Super is a keyword which is used to access the immediate parent class members by using child methods,
+    if both parent and child have the same name.
+    Super is a keyword which is applicable for both variables and methods.
+    super.variablename; --> It refers the immediate parent class member.
+    super.methodname; --> It refers the immediate parent class method.
+    */
+    public static void main(String args[])
+    {
+        SB b=new SB();
+        b.display();
+    }
+}
