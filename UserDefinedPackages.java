de1fda8b6f7d37d62811defad937ec4a20d695d6
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2023-04-10 11:46:50 +0530

    Add files via upload

---
+++ UserDefinedPackages.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import aec.edu.in.Compare;
+public class UserDefinedPackages
+{
+    //package is the first statement to create a user defined package
+    //to see the steps following the execution of package program--> go to SamplePack.java code
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        Compare c=new Compare();
+        int x,y,z;
+        x=sc.nextInt();
+        y=sc.nextInt();
+        z=sc.nextInt();
+        System.out.println(c.greaterValue(x, y, z));
+        System.out.println(c.lesserValue(x, y, z));
+        sc.close();
+    }
+}
